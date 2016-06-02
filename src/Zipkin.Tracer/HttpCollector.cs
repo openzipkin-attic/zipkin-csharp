308e79d910110540eec95cf5665d29d3113ce402
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2016-06-02 18:07:49 +0200

    init commit

---
+++ src/Zipkin.Tracer/HttpCollector.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Thrift.Protocol;
+using Thrift.Transport;
+using Zipkin.Thrift;
+
+namespace Zipkin
+{
+    /// <summary>
+    /// A collector sending all data to zipkin endpoint using HTTP protocol.
+    /// Spans are encoded using Thrift format.
+    /// </summary>
+    public class HttpCollector : ISpanCollector
+    {
+        private Uri _url;
+
+        public HttpCollector(Uri url)
+        {
+            _url = url;
+        }
+
+        public HttpCollector() : this(new Uri("http://localhost:9411/api/v1/spans"))
+        {
+        }
+
+        public async Task<bool> CollectAsync(params Span[] spans)
+        {
+            var serialized = SerializeSpans(spans);
+
+            var request = WebRequest.CreateHttp(_url);
+            request.Method = "POST";
+            request.ContentType = "application/x-thrift";
+            request.ContentLength = serialized.Length;
+            File.WriteAllBytes("ok", serialized);
+            using (var stream = await request.GetRequestStreamAsync())
+            {
+                await stream.WriteAsync(serialized, 0, serialized.Length);
+                await stream.FlushAsync();
+            }
+            using (var reply = (HttpWebResponse)await request.GetResponseAsync())
+            {
+                return reply.StatusCode == HttpStatusCode.Accepted;
+            }
+        }
+
+        private static byte[] SerializeSpans(Span[] spans)
+        {
+            using (var buffer = new TMemoryBuffer())
+            using (var protocol = new TBinaryProtocol(buffer))
+            {
+                protocol.WriteListBegin(new TList(TType.Struct, spans.Length));
+                foreach (var span in spans)
+                {
+                    var thrift = span.ToThrift();
+                    thrift.Write(protocol);
+                }
+                protocol.WriteListEnd();
+                return buffer.GetBuffer();
+            }
+        }
+    }
+}
---
+++ tests/Zipkin.Tracer.Tests/AnnotationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using Xunit;
+using Zipkin.Thrift;
+
+namespace Zipkin.Tracer.Tests
+{
+    public class AnnotationTest
+    {
+        [Fact]
+        public void Annotation_should_serialize_properly()
+        {
+            var annotation = new Annotation("value", new DateTime(2016, 10, 1), new IPEndPoint(IPAddress.Loopback, 1233));
+            var thrifted = annotation.ToThrift();
+
+            Assert.True(thrifted.__isset.host, "Serialized annotation host not set");
+            Assert.Equal(16777343, thrifted.Host.Ipv4); // System.BitConverter.ToInt32(IPAddress.Loopback.GetAddressBytes(), 0)
+            Assert.Equal(annotation.Endpoint.Port, thrifted.Host.Port);
+            Assert.True(thrifted.__isset.value, "Serialized annotation value not set");
+            Assert.Equal(annotation.Value, thrifted.Value);
+            Assert.True(thrifted.__isset.timestamp, "Serialized annotation timestamp not set");
+            Assert.Equal((annotation.Timestamp.Ticks - new DateTime(1970, 1, 1).Ticks) / AnnotationConstants.TicksPerMicosecond, thrifted.Timestamp);
+        }
+    }
+}
